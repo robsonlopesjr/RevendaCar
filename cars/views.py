ac38bbb38c916f52d61fa0694839754cce978e37
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-05-14 20:10:08 -0300

    cadastro de novos carros

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarForm
 
 
 def cars_view(request):
@@ -11,3 +12,16 @@ def cars_view(request):
         cars = cars.filter(model__icontains=search)
 
     return render(request, 'cars.html', {'cars': cars})
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarForm()
+
+    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
