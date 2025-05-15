b800b5a3dab7cdf45bea05757d105793db98e808
Robson Júnior <robsonlopes.jr@gmail.com>
2025-05-15 10:04:55 -0300

    ajustando as views de cars

--- cars/views.py
+++ cars/views.py
@@ -1,39 +1,27 @@
-from django.shortcuts import render, redirect
-from django.views import View
+from django.views.generic import ListView, CreateView
 from cars.models import Car
 from cars.forms import CarForm
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
 
-    search = request.GET.get('search')
+class CarListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    if search:
-        cars = cars.filter(model__icontains=search)
+    # Usando para quando tiver filtros de pesquisa na página
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
 
-    return render(request, 'cars.html', {'cars': cars})
-
-
-class CarView(View):
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-
-        search = request.GET.get('search')
+        search = self.request.GET.get('search')
 
         if search:
             cars = cars.filter(model__icontains=search)
 
-        return render(request, 'cars.html', {'cars': cars})
+        return cars
 
 
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)
-
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarForm()
-
-    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+class NewCarView(CreateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
